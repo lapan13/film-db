f2719981ec9ed70a6cb78bf148cae4c612e798eb
lapan13 <ariirwandi13@gmail.com>
2020-07-29 17:15:19 +0700

    add movie page & movie.js

---
+++ src/script/data/movie.js
@@ -0,0 +1,109 @@
+const movies = [{
+        "popularity": 15.336,
+        "poster_path": null,
+        "id": 671042,
+        "original_title": "Pelukis Hantu",
+        "title": "Pelukis Hantu",
+        "overview": "Plot unknown.",
+        "release_date": "2020-07-30"
+    },
+    {
+        "popularity": 15.309,
+        "poster_path": "/1O1mMvMgz8W0UftJtPaGm9RRjPh.jpg",
+        "id": 723333,
+        "original_title": "Cinta Tanpa Tapi",
+        "title": "Cinta Tanpa Tapi",
+        "overview": "The art blood attracted Arika to Gatta who was passionate about dance. However, her mother did not like her being attracted to the world of dance, especially loving a dancer.",
+        "release_date": "2020-07-30"
+    },
+    {
+        "popularity": 2.033,
+        "id": 638483,
+        "title": "Eyes on Me: The Movie",
+        "release_date": "2020-06-10",
+        "original_title": "아이즈 온 미: 더 무비",
+        "overview": "The first solo concert of K-pop girl group IZ*ONE screened in theaters.",
+        "poster_path": "/xA3HB8LxVQYPIhEB4Gj1VoyVZLB.jpg"
+    },
+    {
+        "popularity": 1.973,
+        "poster_path": "/b1FLdqVEr6R3ZNXqygR6o2WOZ1t.jpg",
+        "id": 681783,
+        "original_title": "Nussa",
+        "title": "Nussa",
+        "overview": "Nussa's life changed when he met a new friend who was much more liked by his friends than himself.",
+        "release_date": "2020-06-25"
+    },
+    {
+        "popularity": 2.18,
+        "id": 683199,
+        "title": "Seperti Hujan yang Jatuh ke Bumi",
+        "release_date": "2020-07-09",
+        "original_title": "Seperti Hujan yang Jatuh ke Bumi",
+        "overview": "Based on the best-selling novel by Boy Candra.",
+        "poster_path": "/zNB8lfyO3YxDhRCvUOE4qmEv42p.jpg"
+    },
+    {
+        "popularity": 1.164,
+        "poster_path": "/neO0c69wGECyhx7XSUyDFXGgvAX.jpg",
+        "id": 664825,
+        "original_title": "Ghibah",
+        "title": "Ghibah",
+        "overview": "Plot unknown.",
+        "release_date": "2020-06-11"
+    },
+    {
+        "popularity": 1.458,
+        "poster_path": "/lck86UMIvIunTassoVGLVTfDsOI.jpg",
+        "id": 681782,
+        "original_title": "Backstage",
+        "title": "Backstage",
+        "overview": "Plot unknown.",
+        "release_date": "2020-06-11"
+    },
+    {
+        "popularity": 1.907,
+        "id": 723561,
+        "title": "Filosofi Kopi: Aroma Gayo",
+        "release_date": "2020-07-11",
+        "original_title": "Filosofi Kopi: Aroma Gayo",
+        "overview": "Rio Dewanto visited the home of Gayo Coffee. He saw firsthand the condition of the land of Gayo, Aceh, and explored the philosophy of a cup of Gayo Coffee.",
+        "poster_path": "/9ynPRDh3JmNCgQ9r2lstO4n8GoP.jpg"
+    },
+    {
+        "popularity": 1.68,
+        "poster_path": "/sLTcezn1wWoeArAPUvnzan0owuv.jpg",
+        "id": 682367,
+        "original_title": "Ashiap Man",
+        "title": "Ashiap Man",
+        "overview": "Plot unknown.",
+        "release_date": "2020-06-18"
+    },
+    {
+        "popularity": 0.838,
+        "id": 721806,
+        "title": "The Virtual Life",
+        "release_date": "2020-07-03",
+        "original_title": "The Virtual Life",
+        "overview": "Take place during COVID-19 pandemic, the story tells about how the situation handled by 3 different people.",
+        "poster_path": "/vMEwTvJO3RuGvknQiNLysnCQQuI.jpg"
+    },
+    {
+        "popularity": 1.287,
+        "id": 709324,
+        "title": "Temporarily, Eternally",
+        "release_date": "2020-06-06",
+        "original_title": "Sementara, Selamanya",
+        "overview": "Saka and Zara decided to stay apart temporarily because Zara was on duty as a doctor who handled COVID-19 patients. Through a series of video calls, the big secret was revealed between them.",
+        "poster_path": "/bomIqeFbQ6SXK3IrMfkxUXFrQh6.jpg"
+    },
+    {
+        "popularity": 1.146,
+        "poster_path": "/kvfkW4RJ8a3iD6mw4ZrNmYP5l1j.jpg",
+        "id": 681784,
+        "original_title": "Adit Sopo Jarwo: The Movie",
+        "title": "Adit Sopo Jarwo: The Movie",
+        "overview": "Plot unknown.",
+        "release_date": "2020-06-25"
+    }
+]
