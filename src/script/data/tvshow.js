059a51f1c25ca574659f520a3cbe43868ea2789c
lapan13 <ariirwandi13@gmail.com>
2020-08-05 12:50:02 +0700

    fix detail on modal

--- src/script/data/tvshow.js
+++ src/script/data/tvshow.js
@@ -1,3 +1,4 @@
+//on load page
 $(document).ready(function () {
     $.ajax({
         type: 'get',
@@ -11,11 +12,15 @@ $(document).ready(function () {
         success: function (result) {
             let tvshow = result.results;
             $.each(tvshow, function (i, data) {
+                //Array of Year
+                let arrYear = [];
+                arrYear.push(data.first_air_date);
+                let year = arrYear.toString().substr(0, 4);
                 $('#movie-list').append(`
-                    <div class="col-md-2 mb-4 ">
-                        <div class="card bg-warning">
-                            <a href="#" class="card-link" data-toggle="modal" data-target=".bd-example-modal-lg">
-                                <img class="card-img-top" src="https://image.tmdb.org/t/p/w185` + data.poster_path + `" alt="` + data.name + `">
+                    <div class="col-md-2 mb-4">
+                        <div class="card bg-warning ">
+                            <a href="#" class="card-link see-detail" data-toggle="modal" data-target=".bd-example-modal-lg" data-id="` + data.id + `">
+                                <img class="card-img-top img-fluid" src="https://image.tmdb.org/t/p/w185` + data.poster_path + `" alt="` + data.name + `">
                             </a>
                         </div>
                     </div>
@@ -25,6 +30,72 @@ $(document).ready(function () {
     });
 });
 
+//see detail modal
+$('#movie-list').on('click', '.see-detail', function () {
+    $.ajax({
+        type: 'get',
+        url: 'https://api.themoviedb.org/3/tv/' + $(this).data('id'),
+        datatype: 'json',
+        data: {
+            'api_key': 'ae0bdd8f96c5b4e84b43e17aa8a01dca',
+            'tv_id': $(this).data('id')
+        },
+        success: function (detail) {
+
+            //Array of Year
+            let arrYear = [];
+            arrYear.push(detail.first_air_date);
+            let year = arrYear.toString().substr(0, 4);
+
+            // Array of Genre
+            let arrGenre = [];
+            $.each(detail.genres, function (i, genre) {
+                arrGenre.push(" " + genre.name);
+                $('.modal-body').html(`
+                    <div class="container-fluid">
+                        <div class="row">
+                            <h5 class="modal-title" id="exampleModalLabel">` + detail.name + ` (` + year + `)</h5>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="https://image.tmdb.org/t/p/w185` + detail.poster_path + `" >
+                            </div>
+                            <div class="col-md-8">
+                                <dl class="row">
+                                    <dt class="col-sm-3">Overview</dt>
+                                        <dd class="col-sm-9">` + detail.overview + `.</dd>
+                                    <dt class="col-sm-3">Genre</dt>
+                                        <dd class="col-sm-9">` + arrGenre + `.</dd>
+                                    <dt class="col-sm-3">Rating</dt>
+                                        <dd class="col-sm-9">` + detail.vote_average + `.</dd>
+                                    <dt class="col-sm-3">Popularity</dt>
+                                        <dd class="col-sm-9">` + detail.popularity + `.</dd>
+                                </dl>
+                                <hr>
+                                <dl class="row">
+                                    
+                                    <dt class="col-sm-3">Seasons</dt>
+                                        <dd class="col-sm-9">` + detail.number_of_seasons + `.</dd>
+                                    <dt class="col-sm-3">Episodes</dt>
+                                        <dd class="col-sm-9">` + detail.number_of_episodes + `.</dd>    
+                                    <dt class="col-sm-3">Country</dt>
+                                        <dd class="col-sm-9">` + detail.origin_country + `.</dd>
+                                    <dt class="col-sm-3">Language</dt>
+                                        <dd class="col-sm-9">` + detail.original_language + `.</dd>
+                                </dl>
+                            </div>
+                        </div>
+                    </div>
+                `);
+            });
+        }
+    });
+});
+
+//when search button clicked
 $('#search-button').on('click', function () {
     $.ajax({
         type: 'get',
@@ -48,7 +119,7 @@ $('#search-button').on('click', function () {
                             <div class="col-md-2 mb-4 ">
                                 <div class="card bg-warning">
                                     <a href="#" class="card-link" data-toggle="modal" data-target=".bd-example-modal-lg">
-                                        <img class="card-img-top" src="https://image.tmdb.org/t/p/w185` + data.poster_path + `" alt="` + data.name + `">
+                                        <img class="card-img-top img-fluid" src="https://image.tmdb.org/t/p/w185` + data.poster_path + `" alt="` + data.name + `">
                                     </a>
                                 </div>
                             </div>   
@@ -58,8 +129,8 @@ $('#search-button').on('click', function () {
                             $('#movie-list').append(`
                                 <div class="col-md-2 mb-4 ">
                                     <div class="card bg-warning">
-                                        <a href="#" class="card-link" data-toggle="modal" data-target=".bd-example-modal-lg">
-                                            <img class="card-img-top" src="../src/img/no-poster.jpg" alt="` + data.name + `">
+                                        <a href="#" class="card-link see-detail" data-toggle="modal" data-target=".bd-example-modal-lg">
+                                            <img class="card-img-top img-fluid" src="../src/img/no-poster.jpg" alt="` + data.name + `">
                                         </a>
                                     </div>
                                 </div>
